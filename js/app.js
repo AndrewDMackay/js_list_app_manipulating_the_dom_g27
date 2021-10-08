8ac992fef759176a4e8ab2d5ef1832bfb04f1c38
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-08 16:30:45 +0100

    4th commit main

--- js/app.js
+++ js/app.js
@@ -1,48 +1,61 @@
 
+// EventListeners..
+
+// addEventListener, main 'DOMContentLoaded', and individual variables tied to querySelector, their own addEventListener, and functions..
 
 document.addEventListener('DOMContentLoaded', () => {
     const newItemform = document.querySelector('#new-item-form');
     newItemform.addEventListener('submit', handleNewItemFormSubmit);
-  
+
     const deleteAllButton = document.querySelector('#delete-all');
     deleteAllButton.addEventListener('click', handleDeleteAllClick);
   })
-  
+ 
+
+  // Add variables, and functions to handle the result of each individual event..
   
   const handleNewItemFormSubmit = function (event) {
     event.preventDefault();
   
-    const readingListItem = createReadingListItem(event.target);
-    const readingList = document.querySelector('#reading-list');
-    readingList.appendChild(readingListItem);
+    const wineListItem = createwineListItem(event.target);
+    const wineList = document.querySelector('#wine-list');
+    wineList.appendChild(wineListItem);
   
     event.target.reset();
   }
   
   
-  const createReadingListItem = function (form) {
-    const readingListItem = document.createElement('li');
-    readingListItem.classList.add('reading-list-item');
+  const createwineListItem = function (form) {
+    const wineListItem = document.createElement('li');
+    wineListItem.classList.add('wine-list-item');
   
-    const title = document.createElement('h2');
-    title.textContent = form.title.value;
-    readingListItem.appendChild(title);
+    const wine = document.createElement('h2');
+    wine.textContent = form.wine.value;
+    wineListItem.appendChild('wine');
   
-    const author = document.createElement('h3');
-    author.textContent = form.author.value;
-    readingListItem.appendChild(author);
+    const producer = document.createElement('h3');
+    producer.textContent = form.producer.value;
+    wineListItem.appendChild('producer');
+
+    const vintage = document.createElement('h4');
+    vintage.textContent = form.vintage.value;
+    wineListItem.appendChild('vintage');
   
-    const category = document.createElement('p');
-    category.textContent = form.category.value;
-    readingListItem.appendChild(category);
+    const type = document.createElement('p');
+    type.textContent = form.type.value;
+    wineListItem.appendChild('type');
+
+    const certification = document.createElement('p');
+    certification.textContent = form.certification.value;
+    wineListItem.appendChild('certification');
   
-    return readingListItem;
+    return wineListItem;
   }
   
   
   const handleDeleteAllClick = function (event) {
-    const readingList = document.querySelector('#reading-list');
-    readingList.innerHTML = '';
+    const wineList = document.querySelector('#wine-list');
+    wineList.innerHTML = '';
   }  
 
 
